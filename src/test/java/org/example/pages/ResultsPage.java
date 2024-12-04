4ae3fd5f108205a14cdd990dcb265e1d97127331
Pro100Dima1 <dima.sinkin@yandex.ru>
2024-12-04 17:08:01 +0300

    Add PageObject pattern

---
+++ src/test/java/org/example/pages/ResultsPage.java
@@ -0,0 +1,45 @@
+package org.example.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ResultsPage {
+    private WebDriver driver;
+
+    @FindBy(css = "#sb_form_q")
+    private WebElement searchField;
+
+    @FindBy(xpath = "//a[contains (@class, 'tilk')]")
+    private List<WebElement> result;
+
+    public void searchPage(int num, String sel) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(6));
+        System.out.println(result.get(num));
+        wait.until(ExpectedConditions.and(
+                ExpectedConditions.elementToBeClickable(result.get(num)),
+                ExpectedConditions.attributeContains(result.get(num), "href", sel)));
+        result.get(num).click();
+        System.out.println("Нажата первая ссылка из списка" + num);
+    }
+
+    public String getCurrentUrll(String url) {
+        ArrayList tabs = new ArrayList<>(driver.getWindowHandles());
+        if (tabs.size() > 1) driver.switchTo().window(tabs.get(1).toString());
+        WebDriverWait waitTwo = new WebDriverWait(driver, Duration.ofSeconds(3));
+        waitTwo.until(ExpectedConditions.urlContains(url));
+        return driver.getCurrentUrl();
+    }
+
+    public ResultsPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+}
---
+++ src/test/java/org/example/tests/BingSearchTest.java
@@ -0,0 +1,49 @@
+package org.example.tests;
+
+import org.example.pages.MainPage;
+import org.example.pages.ResultsPage;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BingSearchTest {
+    private WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+        // Fix the issue https://github.com/SeleniumHQ/selenium/issues/11750
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://www.bing.com/");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @DisplayName("Проверка поисковой строки в поисковике bing")
+    @Test
+    public void searchResultsTest() {
+        String input = "Selenium";
+        MainPage mp = new MainPage(driver);
+        mp.sendText(input);
+
+        ResultsPage rp = new ResultsPage(driver);
+        rp.searchPage(0, input);
+
+        assertEquals("https://www.selenium.dev/", rp.getCurrentUrll(input), "Переход не по первой ссылке списка!!!");
+    }
+
+
+}
